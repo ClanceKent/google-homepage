8297cfd934832a0221fd5fa0c37a660d61b2488a
ClanceKent <clancekent@gmail.com>
2021-06-06 22:56:05 +1000

    Added all html elements, positioned header and footer elements, updated get-country.js to use fetch instead of xmhttp

--- get-country.js
+++ get-country.js
@@ -1,19 +1,31 @@
 
 /*
-Script that guesses the user's country based on timezone supplied by browser
+Script to display the user's country 
 Author - Clance Kent
+
+Uses timezone to get display a country name immediately to avoid flicker,
+then uses an IP lookup service to replace the data to be accurate for countries that share timezones
 */
 
+var contentDiv = document.getElementById("country");
+contentDiv.innerHTML = getCountryByTimezone(); // Prefill country by guessing from browser timezone
+updateCountry("https://ipinfo.io/json"); // Get more accurate country info through IP lookup
 
-
-var tmzn = Intl.DateTimeFormat().resolvedOptions().timeZone;
-
-document.getElementById("country").innerHTML = getCountry(tmzn);
-
-function getCountry(timezone){
+function getCountryByTimezone(){
+    let timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
     if (timezone.includes('/'))
     {
         timezone = timezone.substring(0, timezone.indexOf('/')); 
     }
     return timezone; 
+}
+
+function updateCountry (ipLookupUrl) {
+    fetch(ipLookupUrl)
+    .then(res => res.json())
+    .then((out) => {
+        let regionNames = new Intl.DisplayNames(['en'], {type: 'region'});
+        contentDiv.innerHTML = regionNames.of(out.country);;
+    })
+    .catch(err => { throw err });
 }
