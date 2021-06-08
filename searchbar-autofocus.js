8d9deb244453cb89bfecda177a4c52e9045a6038
ClanceKent <clancekent@gmail.com>
2021-06-08 20:06:14 +1000

    Created images folder

--- searchbar-autofocus.js
+++ searchbar-autofocus.js
@@ -1,3 +1,10 @@
+/*
+Script to give focus to search bar on desktop
+Does the same thing as the autofocus attribute but won't be applied on mobile devices
+
+Author - Clance Kent
+*/
+
 window.onload = function() {
     if (screen.width > 600)
     {
